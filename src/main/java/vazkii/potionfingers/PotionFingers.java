c4ec5b3b3f1b11f349412839f85bc28d9530dcf2
Vasco Lavos <vazkii@hotmail.com>
2017-10-17 19:25:56 +0100

    Add recipes

--- src/main/java/vazkii/potionfingers/PotionFingers.java
+++ src/main/java/vazkii/potionfingers/PotionFingers.java
@@ -1,11 +1,16 @@
 package vazkii.potionfingers;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
+import net.minecraftforge.client.event.GuiScreenEvent.PotionShiftEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import vazkii.arl.recipe.RecipeHandler;
 
 @Mod(modid = PotionFingers.MOD_ID, name = PotionFingers.MOD_NAME, version = PotionFingers.VERSION, dependencies = PotionFingers.DEPENDENCIES)
 public class PotionFingers {
@@ -27,6 +32,25 @@ public class PotionFingers {
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		ring = new ItemRing();
+		
+		RecipeHandler.addOreDictRecipe(new ItemStack(ring), 
+				"LG ", "G G", " G ",
+				'L', new ItemStack(Blocks.LAPIS_BLOCK),
+				'G', "ingotGold");
+		
+		addRingRecipe(MobEffects.SPEED, Items.SUGAR);
+		addRingRecipe(MobEffects.HASTE, "gemEmerald");
+		addRingRecipe(MobEffects.RESISTANCE, "ingotIron");
+		addRingRecipe(MobEffects.JUMP_BOOST, Items.RABBIT_FOOT);
+		addRingRecipe(MobEffects.STRENGTH, Items.BLAZE_POWDER);
+		addRingRecipe(MobEffects.REGENERATION, Items.GHAST_TEAR);
+	}
+	
+	private static void addRingRecipe(Potion potion, Object item) {
+		RecipeHandler.addOreDictRecipe(ItemRing.getRingForPotion(potion), 
+				" I ", "IRI", " I ",
+				'R', new ItemStack(ring),
+				'I', item);
 	}
 	
 }
